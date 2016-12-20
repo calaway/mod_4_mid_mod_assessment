de942cdb38a218021a3cd6bf77cc7ee1f3a2ba56
Calaway <calaway@users.noreply.github.com>
2016-12-19 22:07:40 -0700

    Refactor send_to_hot_links method.

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,12 +3,10 @@ class Link < ActiveRecord::Base
 
   def send_to_hot_links
     conn = Faraday.new(:url => 'https://calawayhotreads.herokuapp.com') do |faraday|
-    # conn = Faraday.new(:url => 'http://0.0.0.0:3001/') do |faraday|
-      faraday.request  :url_encoded             # form-encode POST       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      faraday.request  :url_encoded
+      faraday.adapter  Faraday.default_adapter
     end
-
     params = {read: {url: url}}
-
     response = conn.post '/reads', params
   end
 end
